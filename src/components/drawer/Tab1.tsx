0b7ca6318884bc0222c4305d6951d9e3f950aed6
Grasiela Gomes <produtoradama@gmail.com>
2022-03-19 18:29:29 -0300

    feat: added Tab1 component

--- src/components/drawer/Tab1.tsx
+++ src/components/drawer/Tab1.tsx
@@ -1,11 +1,41 @@
-import { Box, Text, Container } from "@chakra-ui/react";
-import { HomeIcon2 } from "../icons/HomeIcon";
+import {
+  VStack,
+  Box,
+  Text,
+  Container,
+  IconButton,
+  Input,
+} from "@chakra-ui/react";
+import { detailsAddress } from "../../helpers/DetailsAddress";
+import { HomeIcon2, EditIcon } from "../icons";
+import { AddressForm } from "./AddressForm";
 
 export const Tab1 = () => (
-  <Box bg="gray.300" h={200} display="grid" alignItems="center">
-    <Container centerContent>
-      <HomeIcon2 />
-      <Text fontSize="xs">Add photo</Text>
-    </Container>
-  </Box>
+  <VStack spacing={2} align="stratch">
+    <Box bg="gray.300" h={200}>
+      <Box display="flex" justifyContent="end" mb={4}>
+        <IconButton
+          icon={<EditIcon color="blue.300" />}
+          aria-label="Edit icon"
+          variant="ghost"
+          _hover={{ bg: "transparent" }}
+        />
+      </Box>
+      <Container centerContent>
+        <HomeIcon2 />
+        <Text fontSize="xs">Add photo</Text>
+      </Container>
+    </Box>
+    <Input
+      placeholder="Create Name or Project Number"
+      size="md"
+      bg="white"
+      borderRadius="xs"
+    />
+    {detailsAddress.map((address, index) => (
+      <Box key={address}>
+        <AddressForm title={address} sameAsAbove={index !== 0} />
+      </Box>
+    ))}
+  </VStack>
 );
