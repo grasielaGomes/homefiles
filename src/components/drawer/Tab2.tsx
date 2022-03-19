d4f776423ef5389cb5a72a6ee705356e0d77c2f4
Grasiela Gomes <produtoradama@gmail.com>
2022-03-19 20:49:41 -0300

    feat: added Tab2 component

---
+++ src/components/drawer/Tab2.tsx
@@ -0,0 +1,19 @@
+import { RadioGroup, VStack } from "@chakra-ui/react";
+import { useState } from "react";
+import { clientContacts } from "../../helpers/ClientContact";
+import { ContactForm } from "./";
+import { AddressForm } from "./AddressForm";
+
+export const Tab2 = () => {
+  const [value, setValue] = useState("0");
+  return (
+    <VStack spacing={2} align="stratch">
+      {clientContacts.map((contact, index) => (
+        <RadioGroup onChange={setValue} value={value} key={contact}>
+          <ContactForm title={contact} radioValue={String(index)} />
+        </RadioGroup>
+      ))}
+      <AddressForm title="Current Address" />
+    </VStack>
+  );
+};
--- src/components/drawer/index.ts
+++ src/components/drawer/index.ts
@@ -3,3 +3,5 @@ export * from './CustomDrawerHeader';
 export * from './DrawerTab';
 export * from './Tab1';
 export * from './CustomDrawerFooter';
+export * from './Tab2';
+export * from './ContactForm';
