cdb1dc2b05cdef09b4c5f3f89f40c757c8c0107e
Grasiela Gomes <produtoradama@gmail.com>
2022-03-18 21:24:49 -0300

    feat: added MainCard component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,34 @@
-import { Box, Text, Link, VStack, Code, Grid } from "@chakra-ui/react";
-
+import {
+  Box,
+  Flex,
+  Text,
+  VStack,
+  Code,
+  Grid,
+  IconButton,
+} from "@chakra-ui/react";
 import { Header } from "../components/Header";
+import { HomeIcon, MoreIcon, SendIcon } from "../components/icons";
+import { MainCard } from "../components/MainCard";
 
 export const Home = () => (
-  <Box
-    textAlign="start"
-    fontSize="xl"
-    bg="gray.300"
-    minH={1024}
-    py={4}
-    px={6}
-  >
-    <Header />
+  <Box textAlign="start" fontSize="xl" bg="gray.300" minH={1024} py={4} px={6}>
+    <VStack spacing={3} align="stretch">
+      <Header />
+      <Flex gap={2}>
+        <MainCard
+          children={<Text>Hello</Text>}
+          icon={<HomeIcon />}
+          title="My Homefiles"
+          width="75%"
+        />
+        <MainCard
+          children={<Text>Hello</Text>}
+          icon={<SendIcon />}
+          title="Send Document"
+          width="25%"
+        />
+      </Flex>
+    </VStack>
   </Box>
 );
