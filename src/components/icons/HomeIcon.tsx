5587812aede00b942088f86ee3eb97f0be1b58ce
Grasiela Gomes <produtoradama@gmail.com>
2022-03-19 16:45:04 -0300

    feat: added HomeIcon2 component

--- src/components/icons/HomeIcon.tsx
+++ src/components/icons/HomeIcon.tsx
@@ -1,4 +1,7 @@
 import { Icon } from "@chakra-ui/react";
 import { FcHome } from "react-icons/fc";
+import { BiHomeAlt } from "react-icons/bi";
 
 export const HomeIcon = () => <Icon as={FcHome} />;
+
+export const HomeIcon2 = () => <Icon as={BiHomeAlt} w={20} h={20} color="gray.400"/>;
