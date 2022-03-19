5d2549ed2467ef2781c91ddd764d0ace1f973064
Grasiela Gomes <produtoradama@gmail.com>
2022-03-18 21:24:26 -0300

    feat: added MainCardHeader component

--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -1,7 +1,15 @@
 import { Box } from "@chakra-ui/react";
+import { IMainCard } from "../interfaces/MainCard.interface";
+import { MainCardHeader } from "./MainCardHeader";
 
-export const MainCard = () => (
-  <Box bg="gray.100" >
-    
+export const MainCard = ({
+  icon,
+  title,
+  children,
+  width = "100%",
+}: IMainCard) => (
+  <Box bg="gray.100" w={width} boxShadow="base">
+    <MainCardHeader icon={icon} title={title} />
+    {children}
   </Box>
 );
