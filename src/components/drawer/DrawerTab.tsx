adc0975ced1d63bd86bb50972913f8249ce40ae0
Grasiela Gomes <produtoradama@gmail.com>
2022-03-19 22:15:38 -0300

    refactor: changed tab component call

--- src/components/drawer/DrawerTab.tsx
+++ src/components/drawer/DrawerTab.tsx
@@ -1,35 +1,34 @@
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
 import { tabList } from "../../helpers/TabList";
-import { Tab1, Tab2 } from "./";
+import { Tab1, Tab2, Tab3 } from "./";
 
-export const DrawerTab = () => (
-  <Tabs variant="unstyled">
-    <TabList px={4} bg="white">
-      {tabList.map((tab) => (
-        <Tab
-          key={tab}
-          textTransform="uppercase"
-          as="b"
-          fontSize="sm"
-          color="cyan.300"
-          _focus={{ outline: "none" }}
-          _selected={{ color: "cyan.500", borderBottom: "3px solid" }}
-        >
-          {tab}
-        </Tab>
-      ))}
-    </TabList>
+export const DrawerTab = () => {
+  const componentsTab = [<Tab1/>, <Tab2 />, <Tab3 />]
+  return (
+    <Tabs variant="unstyled">
+      <TabList px={4} bg="white">
+        {tabList.map((tab) => (
+          <Tab
+            key={tab}
+            textTransform="uppercase"
+            as="b"
+            fontSize="sm"
+            color="cyan.300"
+            _focus={{ outline: "none" }}
+            _selected={{ color: "cyan.500", borderBottom: "3px solid" }}
+          >
+            {tab}
+          </Tab>
+        ))}
+      </TabList>
 
-    <TabPanels padding={0}>
-      <TabPanel p={0} >
-        <Tab1 />
-      </TabPanel>
-      <TabPanel p={0}>
-        <Tab2 />
-      </TabPanel>
-      <TabPanel>
-        <p>three!</p>
-      </TabPanel>
-    </TabPanels>
-  </Tabs>
-);
+      <TabPanels padding={0}>
+        {componentsTab.map((component, index) => (
+          <TabPanel p={0} key={index}>
+            {component}
+          </TabPanel>
+        ))}
+      </TabPanels>
+    </Tabs>
+  );
+};
