12a267c0887205d3d4d6be67d4f6a1edf25fad90
Grasiela Gomes <produtoradama@gmail.com>
2022-03-20 11:38:32 -0300

    refactor: changed component type into tab3

--- src/components/drawer/Tab3.tsx
+++ src/components/drawer/Tab3.tsx
@@ -9,14 +9,14 @@ export const Tab3 = () => {
   const { height } = useWindowDimensions();
   return (
     <VStack spacing={2} align="stratch" key="tab3">
-      <Box display="flex" justifyContent="end" mt={2}>
+      <Flex justifyContent="end" mt={2}>
         <RoundedButton
           text="team"
           paddingLeft="4"
           leftRadius="full"
           paddingRight="10"
         />
-      </Box>
+      </Flex>
       <Box bg="gray.100" minH={height - 252}>
         <Stack spacing={4} align="stratch" p={4}>
           <Flex gap={2} alignItems="center">
